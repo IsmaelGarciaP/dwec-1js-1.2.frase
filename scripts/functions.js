12b0506a6f67664d93d27143841c667bf091b1f2
Ismael <garciapismael@gmail.com>
2022-09-16 18:07:51 +0200

    commit prueba

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,19 +1,26 @@
 'use strict'
 
 function letras(cadena) {
-
+	return cadena.length;
 }
 
 function palabras(cadena) {
-
+	return cadena.split(' ').length;
 }
 
 function maysc(cadena) {
-
+	return cadena.toLocaleUpperCase();
 }
 
 function titulo(cadena) {
-
+	let palabras = cadena.split(' ');
+	for(let i = 0; i < palabras.length; i++){
+		let palabra = palabras[i];
+		let letra = palabra[0];
+		letra = letra.toLocaleUpperCase;
+		return letra;
+	}
+	return palabras.join(' ');
 }
 
 function letrasReves(cadena) {
@@ -28,7 +35,7 @@ function palindromo(cadena) {
 
 }
 
-module.exports = {
+/*module.exports = {
 	letras,
 	palabras,
 	maysc,
@@ -36,4 +43,4 @@ module.exports = {
 	letrasReves,
 	palabrasReves,
 	palindromo
-}
+}*/
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
 'use strict'
 
-
+let frase = prompt('Dime una Frase');
+console.log(letras(frase) + ' letras y ' + palabras(frase) + ' palabras');
+console.log(maysc(frase));
+console.log(titulo(frase));
